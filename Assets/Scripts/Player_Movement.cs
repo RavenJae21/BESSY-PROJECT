ab6ed73f13c090d26e96767e7c70b5233d96989e
Josh-VGAM <jrodr148@stedwards.edu>
2025-09-25 17:02:01 -0500

    Player movement and Jump completed

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,9 +1,15 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
 public class Player_Movement : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public float jumpForce = 4f;
+    public float chargeForce = 5f;
+
+    public bool isGrounded = false;
 
     private Rigidbody rb; //calling rigibody from unity
 
@@ -14,15 +20,33 @@ public class Player_Movement : MonoBehaviour
 
     void Update()
     {
-        MovePlayer();
+        PlayerMove();
+        PlayerJump();
+        
     }
 
-    public void MovePlayer()
+    public void OnCollisionEnter(Collision collision)
     {
-        float xValue = Input.GetAxis("Horizantal") * Time.deltaTime * moveSpeed;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void PlayerMove()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
+    }
 
+    public void PlayerJump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
