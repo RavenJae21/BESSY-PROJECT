246eda0cb9b8beaa40673cd09a904bbbb9feefd2
Josh-VGAM <jrodr148@stedwards.edu>
2025-10-23 16:44:02 -0500

    lunge is made

--- Assets/Scripts/Player_Charge.cs
+++ Assets/Scripts/Player_Charge.cs
@@ -1,30 +1,36 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Player_Charge : MonoBehaviour
 {
-    public float chargeDamage = 5f;//variable for how much charge damage
+    //lunge dmg, dist, spd
+    public float lungeDamage = 5f;//variable for how much charge damage
+    public float lungeDistance = 0f;//distance of lunge
+    public float lungeSpeed = 0f;//how fast lunge is 
+    public bool isLunge = false;//determines if you are lunging
+
+    //Speed of charge up
     public float currentCharge = 0f;//starting charge
     public float chargeRate = 1f;//how fast it charges up
     public float maxCharge = 100f;//the maximum charge time
-
     public bool isCharging = false;//boolean for knowing if your charging
+    public Slider chargeSlider;
 
     //audio clips
     public AudioSource audioSource;
     public AudioClip mooSound;
 
-    public Slider chargeSlider;
+    public CharacterController controller;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //updates charge meter
         Charge();
         UpdateUI();
     }
@@ -36,20 +42,18 @@ public class Player_Charge : MonoBehaviour
             currentCharge = 0f;
             isCharging = true;
         }
-        if (isCharging && Input.GetMouseButton(0))
+        if (isCharging && Input.GetMouseButton(0))//if let go
         {
             currentCharge += chargeRate * Time.deltaTime;//smoothly charges up
             currentCharge = Mathf.Clamp(currentCharge, 0f, maxCharge);//will not exceed max charge
+
             UpdateUI();
         }
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))//if let go at max charge
         {
             if(currentCharge >= maxCharge)
             {
-                if (audioSource != null && mooSound != null)
-                {
-                    audioSource.PlayOneShot(mooSound);
-                }
+                StartCoroutine(PerformLunge());
             }
             currentCharge = 0f;
             isCharging = false;
@@ -63,5 +67,22 @@ public class Player_Charge : MonoBehaviour
         {
             chargeSlider.value = currentCharge / maxCharge;
         }
+    }
+
+    public IEnumerator PerformLunge()
+    {
+        isLunge = true;
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + (transform.forward * lungeDistance);
+
+        while (Vector3.Distance(transform.position, targetPosition) > 0.1f)//as long as distance between player and target position is greater than 0.1 loop will continue
+        {
+            controller.Move(transform.forward * lungeSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        isLunge = false;
     }
 }
