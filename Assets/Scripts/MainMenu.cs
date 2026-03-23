efe8de3eeee661a0ee47323147837914b96a7bcd
RavenJae21 <ammeza042104@gmail.com>
2026-03-23 12:47:27 -0500

    Control Menu Retcon
    - got rid of control scene (decided to make it into a panel)
    - created canvas prefab
    - gathered code for main menu script to pop up controls menu in the future

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,9 +8,61 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void ControlsMenu() {
+    public void ControlsMenu() 
+    {
         Debug.Log("You clicked Controls");
+
+
+    /*
+    public bool GameIsPaused = false;
+    public KeyCode pauseKey;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true; 
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+    */
+
+
     
+    }
+
+    public void BacktoMainMenu()
+    {
+        SceneManager.LoadScene("Start_Menu");
+        Debug.Log("You clicked Main Menu");
     }
     public void QuitGame()
     {
