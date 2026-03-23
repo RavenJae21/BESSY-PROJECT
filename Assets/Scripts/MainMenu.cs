7dbbb8a1d86342fc7891092af7a5719c170d32cc
RavenJae21 <ammeza042104@gmail.com>
2026-03-23 12:16:09 -0500

    Start/Control Menu Changes
    - got rid of old start menu
    - changed option button to control button
    - updated main menu script to include control menu
    - added WIP Controls Scene Layout
    - CONTROLS BUTTON DOES NOT WORK YET

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,8 +8,8 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void OptionsMenu() {
-        Debug.Log("You clicked Options");
+    public void ControlsMenu() {
+        Debug.Log("You clicked Controls");
     
     }
     public void QuitGame()
